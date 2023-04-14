3322842bb0e2f53132a256bc8c18b76700d9dda7
Aleksander Pugowski <92934224+Pug0r@users.noreply.github.com>
2023-04-14 12:40:37 +0200

    Create OddOccurencesInArray.py

---
+++ Codility/OddOccurencesInArray.py
@@ -0,0 +1,10 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+from collections import Counter
+
+def solution(A):
+    ''' O(n) solution '''
+    counter = Counter(A)
+    for key, value in counter.items():
+        if value % 2 != 0:
+            return key
