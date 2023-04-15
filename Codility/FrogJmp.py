2f642d180a600873ae6da6c13e1a62ef69f2e9b0
Aleksander Pugowski <92934224+Pug0r@users.noreply.github.com>
2023-04-15 14:23:40 +0200

    Create FrogJmp.py

---
+++ Codility/FrogJmp.py
@@ -0,0 +1,10 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X: int, Y: int, D: int) -> int:
+  ''' O(1) solution '''
+    distance = Y - X
+    if distance % D == 0:
+        return distance // D
+    else:
+        return (distance // D) + 1
